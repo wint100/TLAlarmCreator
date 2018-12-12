c3118298d2ab6f2571c48f1bf4b0f6cc429c3751
Mark Wint <Mark.Wint@becbms.com>
2018-12-12 06:20:00 +0000

    Updated Form References

--- TL and Alarm Creator/Form1.cs
+++ TL and Alarm Creator/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TL_and_Alarm_Creator
